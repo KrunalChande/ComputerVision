12c09f471daebe1ac7d8c0cd613197ca964d6b28
krunalchande <krunalchande@gmail.com>
2014-10-03 15:24:44 -0400

    Adding images and initial files

---
+++ HW2/main.m
@@ -0,0 +1,5 @@
+% Implementation of basic stereo algorithm of taking a window around every
+% pixel in one image and search for the best match along the same scan line
+% in the other image.
+
+helperFunctions.SSD
