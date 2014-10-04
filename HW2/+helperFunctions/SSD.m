cb4277ed7e8bf06723d21f647349864be3efd350
krunalchande <krunalchande@gmail.com>
2014-10-04 00:10:35 -0400

    Added and verified SSD

--- HW2/+helperFunctions/SSD.m
+++ HW2/+helperFunctions/SSD.m
@@ -1,18 +1,40 @@
-function SSD(t, x)
+function s_tx = SSD(img,img_template)
 %SSD : This function finds the Sum of Squared Differences Measure(SSD)
 % t = Template window (source)
 % x = proposed location window
-warning('Function not yet tested')
-[tplRows, tplCols] = size(t);
-[imgRows, imgCols] = size(x);
-s_tx = zeros(size(x()));
-for r = 1:imgRows
-    for c = 1:imgCols
-        for j = 1:tplRows
-            for i = 1:tplCols
-                s_tx(r,c) = (t(j,i) - x(r + j- 0.5*tplRows, c + i - 0.5 * tplCols)).^2;
-            end
+
+warning('might give incorrect detection on edges');
+if nargin < 1
+    img = zeros(9,9);
+    img(4:6, 4:6) = 1;
+    img_template = ones(3,3);
+    figure(1), clf;set(gcf,'Name','original img and template');
+    subplot(1,2,1); imshow(img);
+    title('original image');
+    subplot(1,2,2); imshow(img_template);
+    title('image template');
+end
+
+%% set up variables
+[tplRows, tplCols] = size(img_template);
+[imgRows, imgCols] = size(img);
+s_tx = zeros(size(img()));
+
+%% Move template over image one pixel at a time.
+for r = ((tplRows+1)/2:(imgRows-(tplRows-1)/2))
+    for c = (tplCols+1)/2:(imgCols - (tplCols-1)/2)
+        s_tx(r, c) = sum(sum(img_template(1:tplRows, 1:tplCols) ...
+            - img(r-((tplRows-1)/2):r+((tplRows-1)/2), c-((tplCols-1)/2):c+((tplCols-1)/2)).^2));
+        %%
+        if nargin < 1
+            figure, imshow(s_tx);
         end
     end
 end
-end
+maxs_tx = max(max(s_tx));
+s_tx(1:floor(tplRows/2), :) = maxs_tx;
+s_tx(:, 1:floor(tplCols/2)) = maxs_tx;
+
+s_tx(end - floor(tplRows/2):end, :) = maxs_tx;
+s_tx(:, end-floor(tplCols/2):end) = maxs_tx;
+
--- HW2/main.m
+++ HW2/main.m
@@ -2,4 +2,11 @@
 % pixel in one image and search for the best match along the same scan line
 % in the other image.
 
-helperFunctions.SSD
+%% Load and disp the images
+im_l = imread('Data/leftTest.png');
+im_r = imread('Data/rightTest.png');
+figure(1), clf; set(gcf,'Name','Input Images');
+subplot(1,2,1); imshow(im_l);
+subplot(1,2,2); imshow(im_r);
+
+%% Part a: Create disparity image D(x,y) s.t. L(x,y) = R(x + D(x,y),y)
