7aef88b2728ef953dd240d0ff0fcde58b5bc50ac
krunalchande <krunalchande@gmail.com>
2014-10-09 02:43:31 -0400

    Completed problem 2 and 3 of PS2

--- PS2/PS2_3.m
+++ PS2/PS2_3.m
@@ -5,9 +5,23 @@ figureIndex = 1;
 imgLeft = im2double(rgb2gray(imread('Data/proj2-pair1-L.png')));
 imgRight = im2double(rgb2gray(imread('Data/proj2-pair1-R.png')));
 
+imgLeft_withNoise = imnoise(imgLeft,'gaussian',0,0.01);
+
+low_in = min(min(imgLeft));
+high_in = max(max(imgLeft));
+low_out = low_in - 0.2*low_in;
+high_out = high_in + 0.2*high_in;
+if (high_out > 1) 
+    high_out = 1;
+end
+if (low_out < 0) 
+    low_out = 0;
+end
+imgLeft_withContrast =imadjust(imgLeft,[low_in; high_in],[low_out; high_out]); 
+
 figure(figureIndex), clf; set(gcf,'Name','Input Image'); figureIndex = figureIndex + 1;
 subplot(1,2,1), imshow(imgLeft);
-subplot(1,2,2), imshow(imgRight);
+subplot(1,2,2), imshow(imgLeft_withContrast);
 
 maxDisparity = 64;
 windowSize = 5;
@@ -15,18 +29,19 @@ windowSize = 5;
 
 %% Part a
 
-leftDispMap = helperFunctions.SSD(imgLeft, imgRight, windowSize, maxDisparity);
+
+leftDispMap = helperFunctions.SSD(imgLeft_withContrast, imgRight, windowSize, maxDisparity);
 figure(figureIndex), clf; set(gcf,'Name','Left Disparity Map'); figureIndex = figureIndex + 1;
 imshow(leftDispMap, [min(min(leftDispMap)) max(max(leftDispMap))]);
 
-rightDispMap = helperFunctions.SSD(imgRight, imgLeft, windowSize, maxDisparity);
+rightDispMap = helperFunctions.SSD(imgRight, imgLeft_withContrast, windowSize, maxDisparity);
 figure(figureIndex), clf; set(gcf,'Name','Right Disparity Map'); figureIndex = figureIndex + 1;
 imshow(rightDispMap, [min(min(rightDispMap)) max(max(rightDispMap))]);
 
-%% Plot as 3D
+%% Part b
 
-figure(figureIndex), clf; set(gcf,'Name','Left Disparity Map'); figureIndex = figureIndex + 1;
-surf((leftDispMap), 'FaceColor', 'interp', 'EdgeColor', 'none', 'FaceLighting', 'phong');
+% figure(figureIndex), clf; set(gcf,'Name','Left Disparity Map'); figureIndex = figureIndex + 1;
+% surf((leftDispMap), 'FaceColor', 'interp', 'EdgeColor', 'none', 'FaceLighting', 'phong');
 % 
 % 
 % figure(figureIndex), clf; set(gcf,'Name','Right Disparity Map'); figureIndex = figureIndex + 1;
