5b498bdee2cecc7c8e6a9ff24bcf7a157cab7413
krunalchande <krunalchande@gmail.com>
2014-10-09 23:43:36 -0400

    RANSAC 2 working!!

--- PS4/PS4_2.m
+++ PS4/PS4_2.m
@@ -41,7 +41,7 @@ for i  = 1:1
     [ I_x_2, I_y_2] = PS4HelperFunctions.findXYGradients( img(:, :, i+1), windowSize, sigma, filter );
     [Rmax_2, R_2] = PS4HelperFunctions.computeHarrisValue(I_x_2, I_y_2, a);
     [posX_2,posY_2] = PS4HelperFunctions.doNonMaximalSupression(Rmax_2, R_2);
-%     figure(figureIndex),clf,set(gcf,'Name','Harris Corners'); figureIndex = figureIndex + 1;
+    %     figure(figureIndex),clf,set(gcf,'Name','Harris Corners'); figureIndex = figureIndex + 1;
     subplot(1,2,2);
     imshow(img(:, :, i+1),[min(min(img(:, :, i+1))) max(max(img(:, :, i+1)))] ); hold on;
     computedAngle_2  = atan2(I_y_2(posY_2), I_x_2(posX_2));
@@ -61,7 +61,7 @@ for i  = 1:1
     
     [f_1_2,d_1] = vl_sift(I_1,'frames',f_1) ;
     [f_2_2,d_2] = vl_sift(I_2,'frames',f_2) ;
-
+    
     [matches, scores] = vl_ubcmatch(d_1, d_2) ;
     f_1_Index = matches(1,:);
     f_2_Index = matches(2,:);
@@ -69,64 +69,100 @@ for i  = 1:1
     f_2Matched = f_2_2(f_2_Index);
     
     combinedImages = [img(:, :, i) img(:, :, i+1)];
-
-%     figure(figureIndex),clf,set(gcf,'Name','Putative Figure'); figureIndex = figureIndex + 1;
+    
+    %     figure(figureIndex),clf,set(gcf,'Name','Putative Figure'); figureIndex = figureIndex + 1;
     figure(figureIndex),clf,set(gcf,'Name','Putative Figure'); figureIndex = figureIndex + 1;
     imshow(combinedImages,[min(min(img(:, :, i+1))) max(max(img(:, :, i+1)))]); hold on;
     p1 = [posX_1(matches(1,:)) posY_1(matches(1,:))];
     plot(p1(:,1), p1(:,2), 'g*','linewidth',1);
     p2 = [posX_2(matches(2,:)) (posY_2(matches(2,:)))];
-
+    
     p2_shifted = [(posX_2(matches(2,:))+width) (posY_2(matches(2,:)))];
-
+    
     plot(p2_shifted(:,1), p2_shifted(:,2), 'r*','linewidth',1);
-
-   x = [p1(:,1) p2_shifted(:,1)];
-   y = [p1(:,2) p2_shifted(:,2)];
-   warning('selecting some points for display');
-   for j = 1:10:size(matches,2)
-    plot(x(j,:),y(j,:),'Color','b','LineWidth',2)
-   end
-   
-   %% PROBLEM 3 - RANSAC
-   
-   for RANSACIteration = 1:100
-   % Select 2 feature points
-   p1Index = randi(length(p1));
-   p2Index = randi(length(p2));
-   
-   p1_rand = p1(p1Index, :);
-   p2_rand = p2(p2Index, :);
-   
-   % Compute exact similarity transform Hk. eq of line y = mx + c
-   
-   diffX = p2_rand(1) - p1_rand(1);
-   diffY = p2_rand(2) - p1_rand(2);
-   
-   H_k = [1 0 -diffX; 0 1 -diffY];
-   
-
-   % compute inliers where SSD(pi, Hkpi) < e
-   p2_transformed = H_k * [p2 ones(length(p2), 1)]';
-   p2_transformed = p2_transformed';
-   distances = sqrt((p2_transformed(:,2)-p1(:,2)).^2 + (p2_transformed(:,1)-p1(:,1)).^2);
-   
-   % Keep this HK if it is the largest set of inliers
-   inliersInThisH_k = find(distances < threshold);
-   
-   noOfInliers = length(inliersInThisH_k);
-   if (noOfInliers > maxNoOfInliers)
-       H_k_best = H_k
-       noOfInliers
-       maxNoOfInliers = noOfInliers;
-   end
-   
-   end
-   figure,
-   
-   % Recompute least squares estimate to find H on inliers
-   
-   
-   
+    
+    x = [p1(:,1) p2_shifted(:,1)];
+    y = [p1(:,2) p2_shifted(:,2)];
+    warning('selecting some points for display');
+    for j = 1:10:size(matches,2)
+        plot(x(j,:),y(j,:),'Color','b','LineWidth',2)
+    end
+    
+    %% PROBLEM 3 - RANSAC
+    
+    for RANSACIteration = 1:200
+        % Select 2 feature points
+        randIndex = randi(length(p1),2);
+        
+        p1_rand = p1(randIndex, :);
+        p2_rand = p2(randIndex, :);
+        
+        % Compute exact similarity transform Hk. eq of line y = mx + c
+        
+        %    diffX = p2_rand(1) - p1_rand(1);
+        %    diffY = p2_rand(2) - p1_rand(2);
+        %
+        %
+        
+        u1 = p1_rand(1,1);
+        v1 = p1_rand(1,2);
+        u2 = p1_rand(2,1);
+        v2 = p1_rand(2,2);
+        
+        
+        u1_ = p2_rand(1,1);
+        v1_ = p2_rand(1,2);
+        u2_ = p2_rand(2,1);
+        v2_ = p2_rand(2,2);
+        
+        
+        A = [   u1 -v1 1 0 ;...
+            v1  u1 0 1 ;...
+            u2 -v2 1 0 ;...
+            v2  u2 0 1];
+        
+        r = [u1_ ;v1_ ;u2_ ;v2_];
+        
+        x = A\r;
+        a = x(1);
+        b = x(2);
+        c = x(3);
+        d = x(4);
+        
+        H_k = [a -b c; b a d];
+        
+%         % compute inliers where SSD(pi, Hkpi) < e
+%         p2_transformed = H_k * [p2 ones(length(p2), 1)]';
+%         p2_transformed = p2_transformed';
+%         distances = sqrt((p2_transformed(:,2)-p1(:,2)).^2 + (p2_transformed(:,1)-p1(:,1)).^2);
+%         
+%         % Keep this HK if it is the largest set of inliers
+%         inliersInThisH_k = find(distances < threshold);
+%         
+%         noOfInliers1(RANSACIteration) = length(inliersInThisH_k);
+        
+        
+        p1_transformed = H_k * [p1 ones(length(p1), 1)]';
+        p1_transformed = p1_transformed';
+        distances = sqrt((p1_transformed(:,2)-p2(:,2)).^2 + (p1_transformed(:,1)-p2(:,1)).^2);
+        
+        % Keep this HK if it is the largest set of inliers
+        inliersInThisH_k = find(distances < threshold);
+        
+        noOfInliers2(RANSACIteration) = length(inliersInThisH_k);
+        
+        
+        if (noOfInliers2(RANSACIteration) > maxNoOfInliers)
+            H_k_best = H_k
+            noOfInliers2(RANSACIteration)
+            maxNoOfInliers = noOfInliers2(RANSACIteration);
+        end
+        
+    end
+    
+    % Recompute least squares estimate to find H on inliers
+    
+    
+    
 end
 
